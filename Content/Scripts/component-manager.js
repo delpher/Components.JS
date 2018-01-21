ab295b120d2bc8cf47696910c5d107ef87b1a993
Alexey <delph.er@hotmail.com>
2018-01-21 10:45:47 +0200

    Initial commit

---
+++ Content/Scripts/component-manager.js
@@ -0,0 +1,148 @@
+﻿(function() {
+    "use strict";
+    var isRequire = (typeof(require) !== "undefined");
+
+    function ComponentManager() {
+        var self = this;
+
+        self.components = new Array();
+        self.componentBinder = new DummyComponentBinder();
+
+        self.loadComponents = function(target, defaultBinder) {
+            self.componentBinder = defaultBinder ? defaultBinder : self.componentBinder;
+            return bootstrapComponents(target)
+                .then(function(components) {
+                    return handleLoadedComponents(components, target);
+                });
+        }
+
+        function handleLoadedComponents(metas) {
+            var components = new Array();
+
+            metas.forEach(function(meta) {
+                components.push(meta);
+                self.components.push(meta);
+                initializeComponent(meta);
+            });
+
+            return components;
+        }
+
+        function initializeComponent(meta) {
+            var onInit = meta.instance.onInit;
+
+            if (typeof (onInit) === "function")
+                onInit();
+        }
+
+        function bootstrapComponents(target) {
+            var selector = "[data-component]";
+            var containers = target.find(selector).addBack(selector);
+            var loadedComponents = new Array();
+
+            var task = self.jquery.Deferred();
+
+            var promise = self.jquery.when();
+            containers.each(function(index, containerElement) {
+                promise = promise
+                    .then(function() {
+                        return bootstrapComponent(self.jquery(containerElement));
+                    })
+                    .then(function(metadata) {
+                        loadedComponents.push(metadata);
+                    });
+            });
+
+            promise.then(function() { task.resolve(loadedComponents); });
+
+            return task.promise();
+        }
+
+        function bootstrapComponent(container) {
+            var task = self.jquery.Deferred();
+
+            var meta = {
+                name: container.data("component"),
+                container: container
+            };
+
+            if (isRequire) {
+                require([meta.name], function(component) {
+                    handleComponentLoaded(component, task, meta);
+                });
+            } else {
+                var component = eval("window." + meta.name);
+                handleComponentLoaded(component, task, meta);
+            }
+
+            return task.promise();
+        }
+
+        function handleComponentLoaded(component, task, meta) {
+            var instance = createInstance(component);
+            meta.instance = instance;
+            bindComponent(meta)
+                .done(function() { task.resolve(meta) })
+                .fail(function(error) {
+                    error.metadata = meta;
+                    initFailed(error);
+                    task.resolve(meta);
+                    console.error(error, "Component initialization failed.");
+                });
+        }
+
+        function createInstance(component) {
+            if (typeof(component) === "function")
+                return new component;
+            else
+                return component;
+        }
+
+        function bindComponent(meta) {
+            var task = self.jquery.Deferred();
+            var bindingEnabled = true;
+            var ctx = { preventBinding: function () { bindingEnabled = false; } };
+
+            try {
+
+                if (typeof (meta.instance.onBind) === "function") {
+                    meta.instance.onBind(meta.container, ctx);
+                }
+
+                if (bindingEnabled) {
+                    if (typeof (meta.instance.binder) !== "undefined") {
+                        meta.instance.binder.bind(meta);
+                    } else {
+                        self.componentBinder.bind(meta);
+                    }
+                }
+
+                task.resolve();
+            } catch (ex) {
+                task.reject(ex);
+            }
+
+            return task.promise();
+        }
+
+        function initFailed(error) {
+            error.metadata.container.html("Failed to initialize component.");
+        }
+
+        return self;
+    }
+
+    function DummyComponentBinder() {
+        this.bind = function (meta) {}
+    }
+
+    if (isRequire) {
+        define(["jquery"], function(jquery) {
+            ComponentManager.prototype.jquery = jquery;
+            return new ComponentManager();
+        });
+    } else {
+        ComponentManager.prototype.jquery = $;
+        window.ComponentManager = new ComponentManager();
+    }
+})();
---
+++ Content/Scripts/knockout-component-binder.js
@@ -0,0 +1,61 @@
+﻿(function() {
+
+    function KnockoutComponentBinder() {
+        var self = this;
+
+        self.ko.bindingHandlers.stopBinding = {
+            init: function() {
+                return { controlsDescendantBindings: true };
+            }
+        };
+
+        self.bind = function(meta) {
+            var instance = meta.instance;
+            var element = meta.container;
+            var childComponents = element.find("[data-component]");
+
+            childComponents.each(function() {
+                disableBinding(self.jquery(this));
+            });
+
+            self.ko.cleanNode(element[0]);
+            self.ko.applyBindings(instance, element[0]);
+
+            childComponents.each(function() {
+                enableBinding(self.jquery(this));
+            });
+        }
+
+        function disableBinding(node) {
+            var bindings = node.attr("data-bind");
+            node.attr("data-bind-original", bindings);
+            node.attr("data-bind", "stopBinding: true");
+        }
+
+        function enableBinding(node) {
+            var bindings = node.attr("data-bind-original");
+
+            if (bindings !== undefined)
+                node.attr("data-bind", bindings);
+            else
+                node.removeAttr("data-bind");
+
+            node.removeAttr("data-bind-original");
+        }
+
+        return self;
+    }
+
+    if (typeof (define) !== "undefined") {
+        define(["jquery", "knockout"], function(jquery, ko) {
+            KnockoutComponentBinder.prototype.jquery = jquery;
+            KnockoutComponentBinder.prototype.ko = ko;
+            return new KnockoutComponentBinder();
+        });
+    } else {
+        KnockoutComponentBinder.prototype.jquery = $;
+        KnockoutComponentBinder.prototype.ko = ko;
+        window.KnockoutComponentBinder = new KnockoutComponentBinder();
+    }
+
+})();
